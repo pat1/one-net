1bc8eb5dd05b37cee96fd24e58c9dab5deef3793
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2011-01-10 22:49:02 +0000

    Since there are some arrays that determine use some constants defined here for array sizes and we can't have zero-sized arrays, I am changing some values from 0 to 1 so things compile.  This is a temporary fix.  These variables will either return to being equal to 0 or be removed altogether when more #define guards are added and things are fixed, but since I'm doing a little at a time, these values should be positive integers.

--- processors/renesas/src/eval/one_net_eval_hal.h
+++ processors/renesas/src/eval/one_net_eval_hal.h
@@ -59,7 +59,7 @@ enum
     NUM_MASTER_PEER = 8
 #else
     //! Number of peers the MASTER keeps track of
-    NUM_MASTER_PEER = 0
+    NUM_MASTER_PEER = /*0*/1 /* make it 1 for now so things compile.  Can't have a zero size array */
 #endif
 };
 
