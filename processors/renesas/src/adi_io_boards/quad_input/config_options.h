852253fb05b83466ca6dc0fc7856e7dd3bd0789e
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2011-01-16 02:53:18 +0000

    Updating configuration options for Quad Input boards.

--- processors/renesas/src/adi_io_boards/quad_input/config_options.h
+++ processors/renesas/src/adi_io_boards/quad_input/config_options.h
@@ -51,78 +51,23 @@
 //! @{
 
 
+
+// If you do not want to use the config_options.h file to implment the #define
+// values (i.e. you are using an IDE and would rather define things there),
+// make sure you define a #define variable called _OVERRIDE_CONFIG_OPTIONS_FILE.
+
+
+
+#ifndef _OVERRIDE_CONFIG_OPTIONS_FILE
+
+
+// _OVERRIDE_CONFIG_OPTIONS_FILE is not defined.  Use the code below to define things.
+
+
+
 // First undefine everything to be extra careful
 
-// Version Information
-
-#ifdef _ONE_NET_VERSION_1_X
-	#undef _ONE_NET_VERSION_1_X
-#endif
-
-#ifdef _ONE_NET_VERSION_2_X
-	#undef _ONE_NET_VERSION_2_X
-#endif
-
-
-// Main #define options for I/O boards
-#ifndef _ONE_NET_SIMPLE_CLIENT
-	#define _ONE_NET_SIMPLE_CLIENT
-#endif
-
-#ifdef _QUAD_OUTPUT
-	#undef _QUAD_OUTPUT
-#endif
-
-#ifdef _DUAL_OUTPUT
-	#undef _DUAL_OUTPUT
-#endif
-
-#ifdef _QUAD_INPUT
-	#undef _QUAD_INPUT
-#endif
-
-
-// Encryption, Encoding, Random Padding of unused packet portions for
-// increased security, and CRCs
-
-#ifdef _ONE_NET_USE_ENCRYPTION
-	#undef _ONE_NET_USE_ENCRYPTION
-#endif
-
-#ifdef _ONE_NET_USE_ENCODING
-	#undef _ONE_NET_USE_ENCODING
-#endif
-
-#ifdef _ONE_NET_USE_RANDOM_PADDING
-	#undef _ONE_NET_USE_RANDOM_PADDING
-#endif
-
-#ifdef _ONE_NET_USE_CRC
-	#undef _ONE_NET_USE_CRC
-#endif
-
-
-// Peer assignments and polling
-#ifdef _PEER
-	#undef _PEER
-#endif
-
-#ifdef _POLL
-	#undef _POLL
-#endif
-
-
-// Locale for channels (Europe or U.S.A.)
-#ifdef _US_CHANNELS
-	#undef _US_CHANNELS
-#endif
-
-#ifdef _EUROPE_CHANNELS
-	#undef _EUROPE_CHANNELS
-#endif
-
-
-
+#include "undefine_all_defines.h"
 
 
 
@@ -131,7 +76,6 @@
 // Now add any new configuration options you need.  Comment out any you do not need.  #ifdef
 // guards aren't needed since we undefined everything above, but can't hurt so we'll leave them
 // in.
-
 
 // Version Information
 
@@ -144,23 +88,53 @@
 	#define _ONE_NET_VERSION_1_X
 #endif
 
-/*#ifndef _ONE_NET_VERSION_2_X
-	#define _ONE_NET_VERSION_2_X
-#endif*/
+#ifndef _ONE_NET_VERSION_2_X
+//	#define _ONE_NET_VERSION_2_X
+#endif
+
+
+// Master/Client
+
+#ifndef _ONE_NET_MASTER
+	//#define _ONE_NET_MASTER
+#endif
+
+#ifndef _ONE_NET_CLIENT
+	#define _ONE_NET_CLIENT
+#endif
+
+
+
+// Peer Assignments.  Some applications need to implement peer assignments.  Some do not.
+// Define _PEER if your application implements peer assignments.  Default is _PEER assigned.
+// Devices with no inputs (i.e. I/O devices with only relays) cannot have peer assignments.
+#ifndef _PEER
+	#define _PEER
+#endif
+
+
+// Idle Option - Should be defined if the device can ever be idle
+#ifndef _IDLE
+//    #define _IDLE
+#endif
 
 
 // Main #define options for I/O boards
+
+// A device is a "simple client" if and only if it is never used as a master, it cannot
+// implement multi-hop, it cannot implement block messages, and it cannot implement
+// stream messages.
 #ifndef _ONE_NET_SIMPLE_CLIENT
 	#define _ONE_NET_SIMPLE_CLIENT
 #endif
 
-/*#ifndef _QUAD_OUTPUT
-	#define _QUAD_OUTPUT
+#ifndef _QUAD_OUTPUT
+//	#define _QUAD_OUTPUT
 #endif
 
 #ifndef _DUAL_OUTPUT
-	#define _DUAL_OUTPUT
-#endif*/
+//	#define _DUAL_OUTPUT
+#endif
 
 #ifndef _QUAD_INPUT
 	#define _QUAD_INPUT
@@ -172,10 +146,15 @@
 
 // Encryption.  All implementations of ONE-NET must use encryption, but for debugging and
 // learning purposes, it may be useful to turn encryption on and off.  Comment the three
-// lines below out if not using encryption.
+// lines below out if not using encryption.  we already have a variable called
+// ONE_NET_SINGLE_BLOCK_ENCRYPT_NONE, which makes it so encryption is not used, but
+// that may be checked somewhere and cause an error, so I've created a new variable
+// below.  Again, encryption and encoding should only be turned off for debugging/development
+// purposes.  Actual products implementing ONE-NET must be encoded and encrypted.
 #ifndef _ONE_NET_USE_ENCRYPTION
 	#define _ONE_NET_USE_ENCRYPTION
 #endif
+
 
 // Encoding.  All implementations of ONE-NET must use encoding, but for debugging and
 // learning purposes, it may be useful to turn encoding on and off.  Comment the three
@@ -188,15 +167,6 @@
 	#define _ONE_NET_USE_ENCODING
 #endif
 
-// Random Padding.  If defined, unused portions of encrypted packets will be randomly
-// generated. If not turned on, unused portions of encrypted packets will be either 0
-// or "undefined" ("undefined" means that they may be zeroed out, they may be left as-is
-// i.e. whatever is in memory is what is used, or may be randomly generated.  The behavior
-// should not be assumed and is left to the developer).  Note that this option should have
-// no effect on the parsing of packets.  It only affects the creation of packets.
-/*#ifndef _ONE_NET_USE_RANDOM_PADDING
-	#defined _ONE_NET_USE_RANDOM_PADDING
-#endif*/
 
 // CRC.  All implementations of ONE-NET must use CRC's, but for debugging and
 // learning purposes, it may be useful to turn CRC's on and off.  Comment the three
@@ -207,21 +177,26 @@
 #endif
 
 
-// Peer Assignments.  Some applications need to implement peer assignments.  Some do not.
-// Define _PEER if your application implements peer assignments.  Default is _PEER assigned
-#ifndef _PEER
-	#define _PEER
+// Random Padding.  If defined, unused portions of encrypted packets will be randomly
+// generated. If not turned on, unused portions of encrypted packets will be either 0
+// or "undefined" ("undefined" means that they may be zeroed out, they may be left as-is
+// i.e. whatever is in memory is what is used, or may be randomly generated.  The behavior
+// should not be assumed and is left to the developer).  Note that this option should have
+// no effect on the parsing of packets.  It only affects the creation of packets.
+#ifdef _ONE_NET_VERSION_2_X
+    #ifndef _ONE_NET_USE_RANDOM_PADDING
+	    #define _ONE_NET_USE_RANDOM_PADDING
+    #endif
 #endif
 
 
 // Polling - only available for version 2.0 and higher.  Define _POLL if you are using
 // polling.  Default for Version 2.0 is _POLL defined.
-#ifndef _ONE_NET_VERSION_2_X
+#ifdef _ONE_NET_VERSION_2_X
 	#ifndef _POLL
 		#define _POLL
 	#endif
 #endif
-
 
 
 // Locale for channels (Europe or U.S.A.).  At least one locale must be defined.  You can
@@ -237,6 +212,177 @@
 
 
 
+// message type defines
+
+#ifndef _NEED_SWITCH_MESSAGE
+    #define _NEED_SWITCH_MESSAGE
+#endif
+/*
+#ifndef _NEED_PERCENT_MESSAGE
+    #define _NEED_PERCENT_MESSAGE
+#endif
+
+#ifndef _NEED_TEMPERATURE_MESSAGE
+    #define _NEED_TEMPERATURE_MESSAGE
+#endif
+
+#ifndef _NEED_HUMIDITY_MESSAGE
+    #define _NEED_HUMIDITY_MESSAGE
+#endif
+
+#ifndef _NEED_PRESSURE_MESSAGE
+    #define _NEED_PRESSURE_MESSAGE
+#endif
+
+#ifndef _NEED_RAINFALL_MESSAGE
+    #define _NEED_RAINFALL_MESSAGE
+#endif
+
+#ifndef _NEED_SPEED_MESSAGE
+    #define _NEED_SPEED_MESSAGE
+#endif
+
+#ifndef _NEED_DIRECTION_MESSAGE
+    #define _NEED_DIRECTION_MESSAGE
+#endif
+
+#ifndef _NEED_OPENING_MESSAGE
+    #define _NEED_OPENING_MESSAGE
+#endif
+
+#ifndef _NEED_SEAL_MESSAGE
+    #define _NEED_SEAL_MESSAGE
+#endif
+
+#ifndef _NEED_COLOR_MESSAGE
+    #define _NEED_COLOR_MESSAGE
+#endif
+
+#ifndef _NEED_SIMPLE_TEXT_MESSAGE
+    #define _NEED_SIMPLE_TEXT_MESSAGE
+#endif
+
+#ifndef _NEED_DATE_MESSAGE
+    #define _NEED_DATE_MESSAGE
+#endif
+
+#ifndef _NEED_TIME_MESSAGE
+    #define _NEED_TIME_MESSAGE
+#endif
+*/
+#ifndef _NEED_VOLTAGE_MESSAGE
+    #define _NEED_VOLTAGE_MESSAGE
+#endif
+
+#ifndef _NEED_VOLTAGE_SIMPLE_MESSAGE
+    #define _NEED_VOLTAGE_SIMPLE_MESSAGE
+#endif
+/*
+#ifndef _NEED_ENERGY_MESSAGE
+    #define _NEED_ENERGY_MESSAGE
+#endif
+
+#ifndef _NEED_ACCUM_ENERGY_MESSAGE
+    #define _NEED_ACCUM_ENERGY_MESSAGE
+#endif
+
+#ifndef _NEED_PEAK_ENERGY_MESSAGE
+    #define _NEED_PEAK_ENERGY_MESSAGE
+#endif
+
+#ifndef _NEED_GAS_MESSAGE
+    #define _NEED_GAS_MESSAGE
+#endif
+
+#ifndef _NEED_ACCUM_GAS_MESSAGE
+    #define _NEED_ACCUM_GAS_MESSAGE
+#endif
+
+#ifndef _NEED_AVERAGE_GAS_MESSAGE
+    #define _NEED_AVERAGE_GAS_MESSAGE
+#endif
+
+#ifndef _NEED_PEAK_GAS_MESSAGE
+    #define _NEED_PEAK_GAS_MESSAGE
+#endif
+
+#ifndef _NEED_INSTEON_MESSAGE
+    #define _NEED_INSTEON_MESSAGE
+#endif
+
+#ifndef _NEED_X10_MESSAGE
+    #define _NEED_X10_MESSAGE
+#endif
+
+#ifndef _NEED_BLOCK_TEXT_MESSAGE
+    #define _NEED_BLOCK_TEXT_MESSAGE
+#endif*/
+
+
+
+// unit type defines
+#ifndef _NEED_SIMPLE_SWITCH_TYPE
+    #define _NEED_SIMPLE_SWITCH_TYPE
+#endif
+/*
+#ifndef _NEED_DIMMER_SWITCH_TYPE
+    #define _NEED_DIMMER_SWITCH_TYPE
+#endif
+
+#ifndef _NEED_DISPLAY_SWITCH_TYPE
+    #define _NEED_DISPLAY_SWITCH_TYPE
+#endif
+
+#ifndef _NEED_DISPLAY_DIMMER_TYPE
+    #define _NEED_DISPLAY_DIMMER_TYPE
+#endif
+
+#ifndef _NEED_SIMPLE_LIGHT_TYPE
+    #define _NEED_SIMPLE_LIGHT_TYPE
+#endif
+
+#ifndef _NEED_DIMMING_LIGHT_TYPE
+    #define _NEED_DIMMING_LIGHT_TYPE
+#endif
+*/
+#ifndef _NEED_OUTLET_TYPE
+    #define _NEED_OUTLET_TYPE
+#endif
+/*
+#ifndef _NEED_SPEAKER_TYPE
+    #define _NEED_SPEAKER_TYPE
+#endif
+
+#ifndef _NEED_TEMPERATURE_SENSOR_TYPE
+    #define _NEED_TEMPERATURE_SENSOR_TYPE
+#endif
+
+#ifndef _NEED_HUMIDITY_SENSOR_TYPE
+    #define _NEED_HUMIDITY_SENSOR_TYPE
+#endif
+
+#ifndef _NEED_DOOR_WINDOW_SENSOR_TYPE
+    #define _NEED_DOOR_WINDOW_SENSOR_TYPE
+#endif
+
+#ifndef _NEED_MOTION_SENSOR_TYPE
+    #define _NEED_MOTION_SENSOR_TYPE
+#endif
+
+#ifndef _NEED_X10_BRIDGE_TYPE
+    #define _NEED_X10_BRIDGE_TYPE
+#endif
+
+#ifndef _NEED_INSTEON_BRIDGE_TYPE
+    #define _NEED_INSTEON_BRIDGE_SENSOR_TYPE
+#endif
+*/
+
+
+
+// Now test #defines for compatibility
+
+#include "test_defines.h"
 
 
 
@@ -277,4 +423,7 @@
 
 //! @} one_net_config_options
 
+
+#endif // _OVERRIDE_CONFIG_OPTIONS_FILE //
 #endif // _ONE_NET_CONFIG_OPTIONS_H //
+
