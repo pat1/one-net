183d6d741e4d48bc7d38631ec7ba40ede25517d6
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2011-10-28 23:03:55 +0000

    Adding some constants, structures, and a single data message queue.

---
+++ one_net/app/one_net_message.c
@@ -0,0 +1,307 @@
+//! \addtogroup ONE-NET_MESSAGE
+//! @{
+
+/*
+    Copyright (c) 2011, Threshold Corporation
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions, and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of Threshold Corporation (trustee of ONE-NET) nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHEWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*!
+    \file one_net_acknowledge.c
+    \brief Implementation of message functions, including the single essage queue
+
+    Implementation of message functions, including the single essage queue.
+*/
+
+
+#include "config_options.h"
+#include "one_net_types.h"
+#include "one_net_port_const.h"
+#include "one_net_message.h"
+#include "one_net_acknowledge.h"
+#include "one_net_port_specific.h"
+#include "tick.h"
+
+
+//==============================================================================
+//                                  CONSTANTS
+//! \defgroup ONE-NET_MESSAGE_const
+//! \ingroup ONE-NET_MESSAGE
+//! @{
+
+//! @} ONE-NET_MESSAGE_const
+//                                  CONSTANTS END
+//==============================================================================
+
+//==============================================================================
+//                                  TYPEDEFS
+//! \defgroup ONE-NET_MESSAGE_typedefs
+//! \ingroup ONE-NET_MESSAGE
+//! @{
+
+//! @} ONE-NET_MESSAGE_typedefs
+//                                  TYPEDEFS END
+//==============================================================================
+
+
+
+//==============================================================================
+//                      PRIVATE FUNCTION DECLARATIONS
+//! \defgroup ONE-NET_MESSAGE_pri_func
+//! \ingroup ONE-NET_MESSAGE
+//! @{
+
+#if _SINGLE_QUEUE_LEVEL > MED_SINGLE_QUEUE_LEVEL
+static void delete_expired_queue_elements(void);
+#endif
+
+//! @} ONE-NET_MESSAGE_pri_func
+//                      PRIVATE FUNCTION DECLARATIONS END
+//==============================================================================
+
+
+//==============================================================================
+//                              PRIVATE VARIABLES
+//! \defgroup ONE-NET_TIMER_pri_var
+//! \ingroup ONE-NET_TIMER
+//! @{
+
+static UInt8 payload_buffer[SINGLE_DATA_QUEUE_PAYLOAD_BUFFER_SIZE];
+static on_single_data_queue_t single_data_queue[SINGLE_DATA_QUEUE_SIZE];
+static UInt8 single_data_queue_size = 0;
+static UInt16 pld_buffer_tail_idx = 0;
+
+//! @} ONE-NET_MESSAGE_pri_var
+//                              PRIVATE VARIABLES END
+//==============================================================================
+
+
+//==============================================================================
+//                      PUBLIC FUNCTION IMPLEMENTATION
+//! \defgroup ONE-NET_MESSAGE_pub_func
+//! \ingroup ONE-NET_MESSAGE
+//! @{
+
+
+// return true if an element was popped, false otherwise.
+BOOL pop_queue_element(on_single_data_queue_t* const element,
+    UInt8* const buffer, int index)
+{
+    UInt8 i;
+    
+    if(index < 0)
+    {
+        // delete everything
+        single_data_queue_size = 0;
+        pld_buffer_tail_idx = 0;
+        return FALSE;
+    }
+    if(index >= single_data_queue_size)
+    {
+        // index out of range.
+        return FALSE;
+    }
+    
+    // we have a legitimate index.  Pop the element if the caller to the
+    // function provided somewhere to store it.
+    if(element != NULL && buffer != NULL)
+    {
+        one_net_memmove(element, &single_data_queue[index],
+            sizeof(on_single_data_queue_t));
+        element->payload = buffer;
+        one_net_memmove(element->payload, single_data_queue[index].payload,
+            single_data_queue[index].payload_size);
+    }
+    
+    // now delete the element
+    
+    // first delete the payload memory it takes in the buffer.
+    // TODO - more range checking.  Unnecessaray if the queue is doing what
+    // it's supposed to
+    if(index < single_data_queue_size - 1)
+    {
+        // we aren't popping the last element
+        UInt16 this_msg_buffer_start_idx = single_data_queue[index].payload -
+            &payload_buffer[0];
+        UInt16 next_msg_buffer_start_idx = this_msg_buffer_start_idx +
+            single_data_queue[index].payload_size;
+        UInt16 bytes_to_move = pld_buffer_tail_idx - next_msg_buffer_start_idx + 1;
+        one_net_memmove(&payload_buffer[this_msg_buffer_start_idx],
+            &payload_buffer[next_msg_buffer_start_idx], bytes_to_move);
+            
+        // payloads have been moved.  Now adjust the payload pointers
+        for(i = index + 1; index < single_data_queue_size; i++)
+        {
+            single_data_queue[i].payload -= single_data_queue[index].payload_size;
+        }
+    }
+    
+    // adjust the tail index.
+    pld_buffer_tail_idx -= single_data_queue[index].payload_size;
+    
+    // payload(s) have been adjusted.  Now move the queue elements themselves
+    if(index < single_data_queue_size - 1)
+    {
+        one_net_memmove(&single_data_queue[index], &single_data_queue[index + 1],
+            (single_data_queue_size - index - 1) * sizeof(on_single_data_queue_t));
+    }
+    
+    // finally adjust the queue size
+    single_data_queue_size--;
+    return TRUE;
+}
+
+
+/*!
+    \ brief Checks the the single message queue for messages ready to pop
+	
+	If there is a single data transaction in the queue and it's ready to
+	be placed into a transaction, it will be placed into the single transaction spot
+	
+	Any unsent packets that have expired will be deleted.
+	
+	\param[out] next_sleep_time The earliest time the queue could might have something to pop.
+                If the value is 0 and the function returns -1, then the queue is empty and
+                next_pop_time should be disregarded.  If the function returns a non-negative
+                value, next_pop_time should be disregarded.
+	
+	\return index of an element ready to be popped.  If none, -1 is returned.
+	       FALSE otherwise
+*/
+#if _SINGLE_QUEUE_LEVEL > MIN_SINGLE_QUEUE_LEVEL
+int single_data_queue_ready_to_send(tick_t* const next_pop_time)
+{
+	int i, j;
+    int* index;
+    tick_t sleep_time;
+	UInt8 priority = ONE_NET_HIGH_PRIORITY;
+	tick_t cur_tick = get_tick_count();
+	*next_pop_time = 0;
+        
+    #if _SINGLE_QUEUE_LEVEL > MED_SINGLE_QUEUE_LEVEL
+    delete_expired_queue_elements();
+    #endif
+    
+    if(single_data_queue_size == 0)
+    {
+        return -1; // empty queue
+    }
+	
+	// note that send_time equals 0 means send immediately
+
+	
+	// Go through the list and see if any packets are ready.  Try high priority, then low priority.
+	// Note that priority started initialized as HIGH_PRIORITY.  Go through the list twice, once for
+	// each priority.  We'll change priority at the bottom of the outer loop.  If anything is ready
+    // to pop, pop it
+	for(j = 0; j < 2; j++)
+	{
+	    for(i = 0; i < single_data_queue_size; i++)
+	    {
+            if(single_data_queue[i].priority != priority)
+            {
+                continue; // wrong priority
+            }
+            
+		    if(single_data_queue[i].send_time <= cur_tick)
+		    {
+                // we're ready to pop this element.
+                return i;
+		    }
+            
+            sleep_time = single_data_queue[i].send_time - cur_tick;
+            if(*next_pop_time == 0 || sleep_time < *next_pop_time)
+            {
+                *next_pop_time = sleep_time;
+            }
+		}
+		 
+		priority = ONE_NET_LOW_PRIORITY;
+	}
+	
+	return -1; // nothing ready to pop.
+}
+#else
+int single_data_queue_ready_to_send(void)
+{
+    if(single_data_queue_size == 0)
+    {
+        return -1;
+    }
+    
+    if(single_data_queue[0].priority != ONE_NET_HIGH_PRIORITY)
+    {
+        UInt8 i;
+        
+        // go throught the list.  If we have a HIGH priority message, return the index
+        for(i = 1; i < single_data_queue_size; i++)
+        {
+            if(single_data_queue[i].priority == ONE_NET_HIGH_PRIORITY)
+            {
+                return i;
+            }
+        }
+    }
+    
+    return 0;
+}
+#endif
+
+
+//! @} ONE-NET_MESSAGE_pub_func
+//                      PUBLIC FUNCTION IMPLEMENTATION END
+//==============================================================================
+
+//==============================================================================
+//                      PRIVATE FUNCTION IMPLEMENTATION
+//! \addtogroup ONE-NET_MESSAGE_pri_func
+//! \ingroup ONE-NET_MESSAGE
+//! @{
+    
+    
+#if _SINGLE_QUEUE_LEVEL > MED_SINGLE_QUEUE_LEVEL
+static void delete_expired_queue_elements(void)
+{
+    int i;
+    tick_t cur_tick = get_tick_count();
+    for(i = single_data_queue_size - 1; i >= 0 ; i--)
+    {
+        if(single_data_queue[i].expire_time < cur_tick)
+        {
+            pop_queue_element(NULL, NULL, i);
+        }
+    }
+}
+#endif    
+
+
+//! @} ONE-NET_MESSAGE_pri_func
+//                      PRIVATE FUNCTION IMPLEMENTATION END
+//==============================================================================
+
+//! @} ONE-NET_MESSAGE
---
+++ one_net/app/one_net_message.h
@@ -0,0 +1,240 @@
+#ifndef _ONE_NET_MESSAGE_H
+#define _ONE_NET_MESSAGE_H
+
+#include "config_options.h"
+#include "one_net_types.h"
+#include "one_net_packet.h"
+
+
+//! \defgroup ONE-NET_MESSAGE ONE-NET Message Definitions
+//! \ingroup ONE-NET
+//! @{
+
+/*
+    Copyright (c) 2011, Threshold Corporation
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions, and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of Threshold Corporation (trustee of ONE-NET) nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHEWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*!
+    \file one_net_message.h
+    \brief Declarations for ONE-NET messages
+
+    This is global ONE-NET message information.
+*/
+
+
+
+//==============================================================================
+//                                  CONSTANTS
+//! \defgroup ONE-NET_MESSAGE_const
+//! \ingroup ONE-NET_MESSAGE
+//! @{
+
+
+/*!
+    \brief Data Packet Message Types
+*/
+typedef enum
+{
+    ON_APP_MSG,                     //!< Application message type
+    ON_ADMIN_MSG,                   //!< Admin message type
+    ON_INVALID_MSG_TYPE             //!< Indicates the message type is not valid
+} on_msg_type_t;
+
+
+//! Data type being sent/received
+typedef enum
+{
+    ON_NO_TXN,                      //!< No data being sent or received
+    ON_INVITE,                      //!< Invitation packet.
+    ON_SINGLE,                      //!< Sending a single data packet
+    ON_BLOCK,                       //!< Sending a block data packet
+    ON_STREAM,                      //!< Sending a stream data packet
+    ON_RESPONSE                     //!< Response packet
+} on_data_t;
+
+
+/*!
+    \brief Priority Levels
+
+    \Note Changing these may cause the implementation of the Fragment delays
+      admin messages to be changed.
+*/
+typedef enum
+{
+    ONE_NET_NO_PRIORITY,            //!< No current transaction
+    ONE_NET_LOW_PRIORITY,           //!< Low priority transaction
+    ONE_NET_HIGH_PRIORITY,          //!< High priority transaction
+    ONE_NET_SEND_SINGLE_PRIORITY = ONE_NET_HIGH_PRIORITY
+} on_priority_t;
+
+
+//! @} ONE-NET_MESSAGE_const
+//                                  CONSTANTS END
+//==============================================================================
+
+//==============================================================================
+//                                  TYPEDEFS
+//! \defgroup ONE-NET_MESSAGE_typedefs
+//! \ingroup ONE-NET_MESSAGE
+//! @{
+
+
+#ifndef _ONA_MSG_CLASS_T
+#define _ONA_MSG_CLASS_T
+    
+// note : ON_STATUS_QUERY_RESP and ON_STATUS_FAST_QUERY_RESP are subsets of status messages.
+// They just add a little more information, which the application is free to ignore.  In
+// particular, for applications using peer lists where the status of one peer
+// directly results in the changing of the state of another device, generally if, say,
+// a light switch is flipped, an ONA_STATUS_CHANGE message will be sent and any
+// recipient of that message through the peer list will interpret the
+// ONA_STATUS_CHANGE (or possibly all status messages) as a command.  Whether to do
+// that should be considered application specific.  Generally an ONA_STATUS_CHANGE message
+// will be sent when an internal event like a switch flip occurs, ONA_STATUS will be
+// sent when issuing regular time-based status updates, and the "status responses" will
+// be issued when ACKing or NACKing a command or a query of some type.
+
+// Thus if an application wants to treat all status updates the same, it should not check
+// to see whether the message class is ONA_STATUS.  It should instead use the
+// ONA_IS_STATUS_MESSAGE(X) macro as a bit-mask.
+
+
+typedef UInt16 ona_msg_class_t;
+
+
+//!< Status of a unit (not part of an ACK -- or part of an ACk but not
+//!< fit any of the categories below)
+#define ONA_STATUS                 0x0000
+
+//!< Status of a unit has changed(i.e. switch has been flipped)
+#define ONA_STATUS_CHANGE          0x1000
+
+//!< Status of a unit (in the ACK in response to a query)
+#define ONA_STATUS_QUERY_RESP      0x2000
+
+//!< Status of a unit (in the ACK in response to a fast query)
+#define ONA_STATUS_FAST_QUERY_RESP 0x3000
+
+//!< Status of a unit (in the ACK in response to a command)
+#define ONA_STATUS_COMMAND_RESP    0x4000
+
+//!< Command to change status of a unit
+#define ONA_COMMAND                0x5000
+
+//!< Query status of a unit
+#define ONA_QUERY                  0x6000
+
+//!< Fast Query / "poll" status of a unit
+#define ONA_FAST_QUERY             0x7000
+
+// TODO - looks like we have a spare bit here.  Range is 0 to 7, but we have
+// 4 bits reserved.  Shall we reserve only 3 bits instead, leave 8 to 15 for
+// future use, or allow 8 to 15 to be user defined?
+
+
+//!< Used to mask message class bits
+#define ONA_MSG_CLASS_MASK         0xF000
+
+//!< Status Message Macro.  This can be used to quickly ascertain whether he message
+//!< is a status message if all status messages are to treated the same.
+#define ONA_IS_STATUS_MESSAGE(X) ((X & ONA_MSG_CLASS_MASK) <= ONA_STATUS_COMMAND_RESP)
+
+
+#endif // _ONA_MSG_CLASS_T
+  
+
+// TODO - is this the correct spot for on_single queue constants and functions?
+// Should it have its own files?
+
+
+//!< Single Message Data Queue Structure.
+typedef struct
+{
+    UInt8 pid; 
+    UInt8 priority;   
+	on_raw_did_t raw_dst;
+    UInt8 msg_type;
+    UInt8* payload;
+    UInt8 payload_size;
+    #ifdef _PEER
+	BOOL send_to_peer_list;
+    UInt8 src_unit; // relevant only if sending to peer list.
+    #endif
+    #if _SINGLE_QUEUE_LEVEL > MIN_SINGLE_QUEUE_LEVEL
+	tick_t send_time;
+    #endif
+    #if _SINGLE_QUEUE_LEVEL > MED_SINGLE_QUEUE_LEVEL   
+	tick_t expire_time;
+    #endif
+} on_single_data_queue_t;
+
+
+//! @} ONE-NET_MESSAGE_typedefs
+//                                  TYPEDEFS END
+//==============================================================================
+
+//==============================================================================
+//                              PUBLIC VARIABLES
+//! \defgroup ONE-NET_MESSAGE_pub_var
+//! \ingroup ONE-NET_MESSAGE
+//! @{
+
+//! @} ONE-NET_MESSAGE_pub_var
+//                              PUBLIC VARIABLES END
+//==============================================================================
+
+//==============================================================================
+//                      PUBLIC FUNCTION DECLARATIONS
+//! \defgroup ONE-NET_MESSAGE_pub_func
+//! \ingroup ONE-NET_MESSAGE
+//! @{
+
+
+#ifdef _DEBUGGING_TOOLS
+void get_queue_memory(UInt8** pld_buffer, on_single_data_queue_t** queue,
+         UInt8* queue_size, UInt16* tail_idx);
+#endif
+
+BOOL pop_queue_element(on_single_data_queue_t* const element,
+    UInt8* const buffer, int index);
+
+#if _SINGLE_QUEUE_LEVEL > MIN_SINGLE_QUEUE_LEVEL
+int single_data_queue_ready_to_send(tick_t* const queue_sleep_time);
+#else
+int single_data_queue_ready_to_send(void);
+#endif
+
+
+//! @} ONE-NET_MESSAGE_pub_func
+//                      PUBLIC FUNCTION DECLARATIONS END
+//==============================================================================
+
+//! @} ONE-NET_MESSAGE
+
+#endif // _ONE_NET_MESSAGE_H //
