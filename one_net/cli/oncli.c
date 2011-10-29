9302d40b1d3a1d1a47af43c911b540f66b160198
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2011-10-29 02:49:06 +0000

    Adding the skeleton for a command line interface.  Right now just prints.

---
+++ one_net/cli/oncli.c
@@ -0,0 +1,162 @@
+//! \addtogroup oncli
+//! @{
+
+/*
+    Copyright (c) 2011, Threshold Corporation
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions, and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of Threshold Corporation (trustee of ONE-NET) nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHEWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*!
+    \file oncli.c
+    \brief Contains the implementation for the ONE-NET Command Line interface
+*/
+
+
+#include "config_options.h"
+
+
+#include "oncli.h"
+#include "oncli_str.h"
+#include "oncli_port_const.h"
+#include "nprintf.h"
+#include "uart.h"
+
+
+//==============================================================================
+//								CONSTANTS
+//! \defgroup oncli_const
+//! \ingroup oncli
+//! @{
+
+//! @} oncli_const
+//								CONSTANTS END
+//==============================================================================
+
+//==============================================================================
+//								TYPEDEFS
+//! \defgroup oncli_typedefs
+//! \ingroup oncli
+//! @{
+
+//! @} oncli_typedefs
+//								TYPEDEFS END
+//==============================================================================
+
+//==============================================================================
+//							PRIVATE VARIABLES
+//! \defgroup oncli_pri_var
+//! \ingroup oncli
+//! @{
+
+
+//! The string being output
+static char output[ONCLI_MAX_OUTPUT_STR_LEN];
+
+//! @} oncli_pri_var
+//							PRIVATE VARIABLES END
+//==============================================================================
+
+//==============================================================================
+//						PRIVATE FUNCTION DECLARATIONS
+//! \defgroup oncli_pri_func
+//! \ingroup oncli
+//! @{
+
+
+//! @} oncli_pri_func
+//						PRIVATE FUNCTION DECLARATIONS END
+//==============================================================================
+
+//==============================================================================
+//						PUBLIC FUNCTION IMPLEMENTATION 
+//! \defgroup oncli_pub_func
+//! \ingroup oncli
+//! @{
+
+
+/*!
+    \brief Sends a message out of the serial port.
+    
+    The std library provided by Renesas does not provide output functions that
+    
+
+    \param[in] FMT Format of the string to send.
+    \param[in] ... Optional parameters to put use in the final string based on
+      FMT.
+
+    \return void
+*/
+void oncli_send_msg(const char * const FMT, ...)
+{
+    va_list ap;
+    int output_len;
+
+    if(!FMT)
+    {
+        return;
+    } // if the parameter is invalid //
+
+    va_start(ap, FMT);    
+    if((output_len = vsnprintf(output, sizeof(output), FMT, ap))
+      > sizeof(output))
+    {
+        if((output_len = snprintf(output, sizeof(output),
+          ONCLI_OUTPUT_STR_TOO_SHORT_FMT, (UInt16)sizeof(output), output_len))
+          > sizeof(output))
+        {
+            uart_write(ONCLI_FATAL_ERR_1_STR,ONCLI_FATAL_ERR_1_LEN);
+            while(uart_tx_bytes_free() < uart_tx_buffer_size());
+            return;
+            // EXIT();
+        } // if the output string is still to short //
+    } // if the output string is too short //
+
+    va_end(ap);
+    uart_write(output, output_len);
+} // oncli_send_msg //
+
+
+
+//! @} oncli_pub_func
+//						PUBLIC FUNCTION IMPLEMENTATION END
+//==============================================================================
+
+//==============================================================================
+//						PRIVATE FUNCTION IMPLEMENTATION 
+//! \addtogroup oncli_pri_func
+//! \ingroup oncli
+//! @{
+
+
+//! @} oncli_pri_func
+//						PRIVATE FUNCTION IMPLEMENTATION END
+//==============================================================================
+
+
+//! @} oncli
+
---
+++ one_net/cli/oncli.h
@@ -0,0 +1,97 @@
+#ifndef _ONCLI_H 
+#define _ONCLI_H 
+
+
+//! \defgroup oncli ONE-NET Command Line Interface
+//! @{
+
+/*
+    Copyright (c) 2011, Threshold Corporation
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions, and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of Threshold Corporation (trustee of ONE-NET) nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHEWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*!
+    \file oncli.h
+
+    \brief Contains declarations for ONE-NET Command Line Interface
+      functionality
+
+    The ONE-NET Command Line Interface is an ASCII protocol designed for the
+    ONE-NET evaluation boards so a user can easily test and evaluate the
+    ONE-NET protocol (MAC layer).
+*/
+
+
+//==============================================================================
+//								CONSTANTS
+//! \defgroup oncli_const 
+//! \ingroup oncli
+//! @{
+
+//! @} oncli_const
+//								CONSTANTS END
+//==============================================================================
+
+//==============================================================================
+//								TYPEDEFS 
+//! \defgroup oncli_typedefs
+//! \ingroup oncli
+//! @{
+
+//! @} oncli_typedefs
+//								TYPEDEFS END
+//==============================================================================
+
+//==============================================================================
+//                              PUBLIC VARIABLES
+//! \defgroup oncli_pub_var
+//! \ingroup oncli
+//! @{
+
+//! @} oncli_pub_var
+//                              PUBLIC VARIABLES END
+//==============================================================================
+
+//==============================================================================
+//						PUBLIC FUNCTION DECLARATIONS
+//! \defgroup oncli_pub_func
+//! \ingroup oncli
+//! @{
+
+
+void oncli_send_msg(const char * const FMT, ...);
+
+
+//! @} oncli_pub_func
+//						PUBLIC FUNCTION DECLARATIONS END
+//==============================================================================
+
+//! @} oncli
+
+
+#endif // #ifdef _ONCLI_H //
