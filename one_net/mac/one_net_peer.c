f7a1e9761bdb310e00df25d27a8afc2e425256c4
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2011-10-29 05:55:43 +0000

    Adding some peer handling structures.

---
+++ one_net/mac/one_net_peer.c
@@ -0,0 +1,170 @@
+//! \addtogroup ONE-NET_PEER ONE-NET PEER functionality
+//! \ingroup ONE-NET_PEER
+//! @{
+
+/*
+    Copyright (c) 2011, Threshold Corporation
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions, and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of Threshold Corporation (trustee of ONE-NET) nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHEWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*!
+    \file one_net_peer.c
+    \brief ONE-NET peer functionality implementation.
+
+    Handles peer assignments functionality
+    
+    \note See one_net.h for the version of the ONE-NET source as a whole.  If
+      any one file is modified, the version number in one_net.h will need to be
+      updated.
+*/
+
+#include "config_options.h"
+
+#ifdef _PEER
+
+
+#include "one_net_types.h"
+#include "one_net_peer.h"
+#include "one_net_constants.h"
+#include "one_net_status_codes.h"
+
+
+//==============================================================================
+//                                  CONSTANTS
+//! \defgroup ONE-NET_PEER_const
+//! \ingroup ONE-NET_PEER
+//! @{
+
+
+// same as ON_ENCODED_BROADCAST_DID
+const on_encoded_did_t INVALID_PEER = {0xB4, 0xB4};
+
+
+
+//! @} ONE-NET_PEER_const
+//                                  CONSTANTS END
+//==============================================================================
+
+//==============================================================================
+//                                  TYPEDEFS
+//! \defgroup ONE-NET_PEER_typedefs
+//! \ingroup ONE-NET_PEER
+//! @{
+
+
+//! @} ONE-NET_PEER_typedefs
+//                                  TYPEDEFS END
+//==============================================================================
+
+//==============================================================================
+//                              PRIVATE VARIABLES
+//! \defgroup ONE-NET_PEER_pri_var
+//! \ingroup ONE-NET_PEER
+//! @{
+
+//! @} ONE-NET_PEER_pri_var
+//                              PRIVATE VARIABLES END
+//==============================================================================
+
+
+//==============================================================================
+//                              PUBLIC VARIABLES
+//! \defgroup ONE-NET_PEER_pub_var
+//! \ingroup ONE-NET_PEER
+//! @{
+
+UInt8 peer_storage[PEER_STORAGE_SIZE_BYTES];
+
+on_peer_unit_t* const peer = (on_peer_unit_t* const) &peer_storage[0];
+peer_msg_mgr_t peer_msg_mgr;
+
+
+
+//! @} ONE-NET_PEER_pub_var
+//                              PUBLIC VARIABLES END
+//==============================================================================
+
+//==============================================================================
+//                      PRIVATE FUNCTION DECLARATIONS
+//! \defgroup ONE-NET_PEER_pri_func
+//! \ingroup ONE-NET_PEER
+//! @{
+
+
+//! @} ONE-NET_PEER_pri_func
+//                      PRIVATE FUNCTION DECLARATIONS END
+//==============================================================================
+
+//==============================================================================
+//                      PUBLIC FUNCTION IMPLEMENTATION
+//! \defgroup ONE-NET_PEER_pub_func
+//! \ingroup ONE-NET_PEER
+//! @{
+
+
+/*!
+    \brief Resets the peer memory.
+    
+    This should be called by the device when the device's init function is
+    called or anytime the device peer memory is to be wiped out.
+    
+    \return ONS_SUCCESS if the peer memory was reset
+            ONS_FAILURE otherwise
+*/
+one_net_status_t one_net_reset_peers(void)
+{
+    on_peer_unit_t empty_peer = {{0xB4, 0xB4}, ONE_NET_DEV_UNIT,
+      ONE_NET_DEV_UNIT};
+    one_net_memset_block(peer, sizeof(empty_peer),
+      ONE_NET_MAX_PEER_UNIT, &empty_peer);      
+    return ONS_SUCCESS;
+} // one_net_reset_peers //
+
+
+//! @} ONE-NET_PEER_pub_func
+//                      PUBLIC FUNCTION IMPLEMENTATION END
+//==============================================================================
+
+
+
+//==============================================================================
+//                      PRIVATE FUNCTION IMPLEMENTATION
+//! \defgroup ONE-NET_PEER_pri_func
+//! \ingroup ONE-NET_PEER
+//! @{
+
+
+//! @} ONE-NET_PEER_pri_func
+//                      PRIVATE FUNCTION IMPLEMENTATION END
+//==============================================================================
+
+
+#endif // if _PEER is defined //
+
+
+//! @} ONE-NET_PEER
---
+++ one_net/mac/one_net_peer.h
@@ -0,0 +1,158 @@
+#ifndef _ONE_NET_PEER_H
+#define _ONE_NET_PEER_H
+
+//! \defgroup ONE-NET_PEER ONE-NET PEER functionality.
+//! \ingroup ONE-NET_PEER
+//! @{
+
+/*
+    Copyright (c) 2011, Threshold Corporation
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions, and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of Threshold Corporation (trustee of ONE-NET) nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHEWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*!
+    \file one_net_peer.h
+    \brief ONE-NET Peer functionality declarations.
+
+    Handles everything dealing with peer assignments
+    
+    \note See one_net.h for the version of the ONE-NET source as a whole.  If
+      any one file is modified, the version number in one_net.h will need to be
+      updated.
+*/
+
+#include "config_options.h"
+
+#ifdef _PEER
+
+#include "one_net_types.h"
+#include "one_net_port_specific.h"
+#include "one_net_constants.h"
+#include "one_net_status_codes.h"
+#include "one_net_application.h"
+#include "one_net_port_const.h"
+
+
+//==============================================================================
+//                                  CONSTANTS
+//! \defgroup ONE-NET_PEER_const
+//! \ingroup ONE-NET_PEER
+//! @{
+
+
+extern const on_encoded_did_t INVALID_PEER;
+
+
+//! @} ONE-NET_PEER_const
+//                                  CONSTANTS END
+//==============================================================================
+
+//==============================================================================
+//                                  TYPEDEFS
+//! \defgroup ONE-NET_PEER_typedefs
+//! \ingroup ONE-NET_PEER
+//! @{
+
+
+/*!
+    \brief Represents a peer for a unit
+*/
+typedef struct
+{
+    //! did of the of the peer
+    on_encoded_did_t peer_did;
+
+    //! The unit in this device that triggers a message to a peer.
+    UInt8 src_unit;
+	
+    //! The unit in the peer that src_unit sends to.
+    UInt8 peer_unit;
+} on_peer_unit_t;
+
+
+enum
+{
+    PEER_STORAGE_SIZE_BYTES = ONE_NET_MAX_PEER_UNIT * sizeof(on_peer_unit_t)
+};
+
+
+/*!
+    \brief Manages messages sent to the peers of a given source unit.
+*/
+typedef struct
+{
+    //! The source unit for the message.  A value of ONE_NET_DEV_UNIT indicates that
+    //! it is not in use.
+    UInt8 src_unit;
+    
+    //! Where we currently are when iterating through the peer unit list.
+    UInt8 current_idx;
+    
+    //! Index in the message that contains the destination did that needs to
+    //! be changed when the message is sent to the next peer.  A value of
+    //! ON_MAX_RAW_PLD_LEN indicates that the message is not to be changed.
+    UInt8 msg_dst_unit_idx;
+} peer_msg_mgr_t;
+
+
+//! @} ONE-NET_PEER_typedefs
+//                                  TYPEDEFS END
+//==============================================================================
+
+//==============================================================================
+//                              PUBLIC VARIABLES
+//! \defgroup ONE-NET_PEER_pub_var
+//! \ingroup ONE-NET_PEER
+//! @{
+
+
+extern UInt8 peer_storage[];
+extern on_peer_unit_t* const peer;
+
+
+//! @} ONE-NET_PEER_pub_var
+//                              PUBLIC VARIABLES END
+//==============================================================================
+
+//==============================================================================
+//                      PUBLIC FUNCTION DECLARATIONS
+//! \defgroup ONE-NET_PEER_pub_func
+//! \ingroup ONE-NET_PEER
+//! @{
+
+
+one_net_status_t one_net_reset_peers(void);
+
+
+//! @} ONE-NET_PEER_pub_func
+//                      PUBLIC FUNCTION DECLARATIONS END
+//==============================================================================
+
+//! @} ONE-NET_PEER
+#endif // _ONE_NET_PEER defined //
+#endif // _ONE_NET_PEER_H //
