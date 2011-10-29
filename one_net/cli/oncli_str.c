d3f8784f0ca56e3da2a525b08eaf0fa9135f2e9e
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2011-10-29 02:48:14 +0000

    Adding some string constants.

---
+++ one_net/cli/oncli_str.c
@@ -0,0 +1,117 @@
+//! \addtogroup oncli_str
+//! @{
+
+/*
+    Copyright (c) 2011, Threshold Corporation
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions, and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of Threshold Corporation (trustee of ONE-NET) nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHEWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*!
+    \file oncli_str.c
+
+    \brief Contains string constants associated with the ONE-NET Command Line
+      interface.
+
+    These strings are to be used in a case insensitive manner.
+*/
+
+#include "oncli_str.h"
+
+
+//==============================================================================
+//								CONSTANTS
+//! \defgroup oncli_str_const
+//! \ingroup oncli_str
+//! @{
+
+
+// Error formats
+//! Format to indicate the output string is too short
+const char * const ONCLI_OUTPUT_STR_TOO_SHORT_FMT
+  = "Buffer is not big enough; have %u, needed %u\n";
+
+//! Fatal error 1
+const char * const ONCLI_FATAL_ERR_1_STR = "\nFATAL ERROR: 1\n";
+
+
+//! @} oncli_str_const
+//								CONSTANTS END
+//==============================================================================
+
+//==============================================================================
+//								TYPEDEFS
+//! \defgroup oncli_str_typedefs
+//! \ingroup oncli_str
+//! @{
+
+//! @} oncli_str_typedefs
+//								TYPEDEFS END
+//==============================================================================
+
+//==============================================================================
+//                              PRIVATE VARIABLES
+//! \defgroup oncli_str_pri_var
+//! \ingroup oncli_str
+//! @{
+
+//! @} oncli_str_pri_var
+//                              PRIVATE VARIABLES END
+//==============================================================================
+
+//==============================================================================
+//						PRIVATE FUNCTION DECLARATIONS
+//! \defgroup oncli_str_pri_func
+//! \ingroup oncli_str
+//! @{
+
+//! @} oncli_str_pri_func
+//						PRIVATE FUNCTION DECLARATIONS END
+//==============================================================================
+
+//==============================================================================
+//						PUBLIC FUNCTION IMPLEMENTATION
+//! \defgroup oncli_str_pub_func
+//! \ingroup oncli_str
+//! @{
+
+
+//! @} oncli_str_pub_func
+//						PUBLIC FUNCTION IMPLEMENTATION END
+//==============================================================================
+
+//==============================================================================
+//						PRIVATE FUNCTION IMPLEMENTATION
+//! \defgroup oncli_str_pri_func
+//! \ingroup oncli_str
+//! @{
+
+//! @} oncli_str_pri_func
+//						PRIVATE FUNCTION IMPLEMENTATION END
+//==============================================================================
+
+//! @} oncli_str
---
+++ one_net/cli/oncli_str.h
@@ -0,0 +1,106 @@
+#ifndef _ONCLI_STR_H
+#define _ONCLI_STR_H
+
+
+//! \defgroup oncli_str ONE-NET Command Line Interface Strings
+//! \ingroup oncli
+//! @{
+
+/*
+    Copyright (c) 2011, Threshold Corporation
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+        * Redistributions of source code must retain the above copyright notice,
+          this list of conditions, and the following disclaimer.
+        * Redistributions in binary form must reproduce the above copyright
+          notice, this list of conditions and the following disclaimer in the
+          documentation and/or other materials provided with the distribution.
+        * Neither the name of Threshold Corporation (trustee of ONE-NET) nor the
+          names of its contributors may be used to endorse or promote products
+          derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+    CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+    MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+    BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+    OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+    BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+    LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+    NEGLIGENCE OR OTHEWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/*!
+    \file oncli_str.h
+
+    \brief Contains string constants associated with the ONE-NET Command Line
+      interface.
+
+    These strings are to be used in a case insensitive manner.
+*/
+
+
+
+//==============================================================================
+//								CONSTANTS
+//! \defgroup oncli_str_const
+//! \ingroup oncli_str
+//! @{
+    
+
+// Error Formats
+extern const char * const ONCLI_OUTPUT_STR_TOO_SHORT_FMT;
+
+
+// Error Strings
+extern const char * const ONCLI_FATAL_ERR_1_STR;
+enum
+{
+    ONCLI_FATAL_ERR_1_LEN = 15
+};
+
+
+//! @} oncli_str_const
+//								CONSTANTS END
+//==============================================================================
+
+//==============================================================================
+//								TYPEDEFS
+//! \defgroup oncli_str_typedefs
+//! \ingroup oncli_str
+//! @{
+
+//! @} oncli_str_typedefs
+//								TYPEDEFS END
+//==============================================================================
+
+//==============================================================================
+//                              PUBLIC VARIABLES
+//! \defgroup oncli_str_pub_var
+//! \ingroup oncli_str
+//! @{
+
+//! @} oncli_str_pub_var
+//                              PUBLIC VARIABLES END
+//==============================================================================
+
+//==============================================================================
+//						PUBLIC FUNCTION DECLARATIONS
+//! \defgroup oncli_str_pub_func
+//! \ingroup oncli_str
+//! @{
+
+//! @} oncli_str_pub_func
+//						PUBLIC FUNCTION DECLARATIONS END
+//==============================================================================
+
+//! @} oncli_str
+
+
+#endif // #ifdef _ONCLI_STR_H //
