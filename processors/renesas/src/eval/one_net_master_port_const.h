dd8986f184413284e7990ac74dc11d2d44c72626
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2011-11-11 14:21:59 +0000

    Changing some values that will not fit inside one byte from enumerations to #defines.

--- processors/renesas/src/eval/one_net_master_port_const.h
+++ processors/renesas/src/eval/one_net_master_port_const.h
@@ -61,19 +61,21 @@ enum
 {
     //! The number of CLIENTS the MASTER keeps track of.
     ONE_NET_MASTER_MAX_CLIENTS = 5,
-
-    //! Frequency in ms to send new CLIENT Invite
-    ONE_NET_MASTER_INVITE_SEND_TIME = 250,
-    
-    //! The number of ms to poll a given channel to see if it is clear when a
-    //! network is created for the first time.
-    ONE_NET_MASTER_NETWORK_CHANNEL_CLR_TIME = 5000,
-    
-    //! The number of ticks to wait before deciding all channels viewed so far
-    //! are busy and to lower the channel clear time to try and find the least
-    //! busy channel.
-    ONE_NET_MASTER_CHANNEL_SCAN_TIME = 10000
 };
+
+
+// timers
+//! Frequency in ms to send new CLIENT Invite
+#define ONE_NET_MASTER_INVITE_SEND_TIME 250
+
+//! The number of ms to poll a given channel to see if it is clear when a
+//! network is created for the first time.
+#define ONE_NET_MASTER_NETWORK_CHANNEL_CLR_TIME 5000
+
+//! The number of ticks to wait before deciding all channels viewed so far
+//! are busy and to lower the channel clear time to try and find the least
+//! busy channel.
+#define ONE_NET_MASTER_CHANNEL_SCAN_TIME 10000
 
 
 
