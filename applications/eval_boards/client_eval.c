411ea4531a38587cada88a93b70912184ed58cd4
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2011-12-27 12:13:04 +0000

    Fix printout.

--- applications/eval_boards/client_eval.c
+++ applications/eval_boards/client_eval.c
@@ -131,7 +131,7 @@ void one_net_client_client_added(const on_raw_did_t * const raw_did,
     else
     {
         oncli_send_msg("Device %03d has been added to the network.\n",
-          raw_did);
+          did_to_u16(raw_did));
     }
 }
 
