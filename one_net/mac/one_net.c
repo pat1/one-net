2339a0b08fa66bc3e289e8751594facfe2a33104
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2012-03-29 05:33:47 +0000

    Another bug fix.

--- one_net/mac/one_net.c
+++ one_net/mac/one_net.c
@@ -3211,7 +3211,7 @@ on_message_status_t rx_block_data(on_txn_t* txn, block_stream_msg_t* bs_msg,
         }
     }
 
-    if(block_get_lowest_unsent_index(block_pkt->sent, expected_chunk_size) == -1)
+    if(block_get_lowest_unsent_index(bs_msg->sent, expected_chunk_size) == -1)
     {
         // chunk has been received.
         #if !defined(_ONE_NET_MASTER)
