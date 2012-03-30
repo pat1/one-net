a7261e0f27075206e6e7dcca9f7ee8ff6fbb89af
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2012-03-30 15:15:54 +0000

    Block / stream termination flags were not being reset properly so the first message was accepted, but not the second.

--- one_net/mac/one_net_client.c
+++ one_net/mac/one_net_client.c
@@ -1940,6 +1940,7 @@ static on_message_status_t handle_admin_pkt(const on_encoded_did_t * const
             {
                 bs_msg.transfer_in_progress = TRUE;
                 bs_msg.byte_idx = 0;
+                bs_msg.bs_on_state = ON_LISTEN_FOR_DATA;
                 
                 // Set the block / stream timer to the timeout
                 ont_set_timer(ONT_BS_TIMER, MS_TO_TICK(bs_msg.timeout));
