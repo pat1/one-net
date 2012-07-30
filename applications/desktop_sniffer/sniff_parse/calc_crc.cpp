dafdd525fac70c1045d2d5d22fa564c9ffe9f3a4
onenet <onenet@6aaea63c-a731-418f-8826-0f8392395df9>
2012-07-30 16:11:35 +0000

    Implementing the calc_crc.cpp program.

--- applications/desktop_sniffer/sniff_parse/calc_crc.cpp
+++ applications/desktop_sniffer/sniff_parse/calc_crc.cpp
@@ -1,9 +1,47 @@
 #include "parse_utility_args.h"
+#include "string_utils.h"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <iomanip>
+#include <stdint.h>
+#include "string_utils.h"
+#include "one_net_crc.h"
+#include "one_net_status_codes.h"
+using namespace std;
 
 
-//encode_value decode_value encode_array decode_array encrypt_array decrypt_array encode_did decode_did encode_nid decode_nid calc_crc dec_to_hex hex_to_dec
-
-int main(int argc, char* args[])
+void usage()
 {
+    cout << "Usage: ./calc_crc -i 0x33445566778899 ---> Computes 8-bit ONE-NET CRC over 0x33445566778899\n";
+    cout << "Usage: ./calc_crc -if array.txt ---> Computes 8-bit ONE-NET CRC over an array stored in array.txt\n";
+}
+
+
+int main(int argc, char* argv[])
+{
+    string error;
+    UInt8 array_bytes[255];
+    UInt8 num_array_bytes = sizeof(array_bytes);
+
+    if(!ParseArgsForInputArray(argc, argv, array_bytes, num_array_bytes,
+      error))
+    {
+        cout << "Could not parse input array.  Error = " << error << endl;
+        usage();
+        exit(0);
+    }
+
+    if(num_array_bytes == 0)
+    {
+        cout << "Number of bytes in input array must be positive." << endl;
+        usage();
+        exit(0);
+    }
+
+    UInt8 crc = (UInt8) one_net_compute_crc(array_bytes, num_array_bytes,
+      ON_PLD_INIT_CRC, ON_PLD_CRC_ORDER);
+
+    cout << "0x" << hex << (int) crc;
     return 0;
 }
